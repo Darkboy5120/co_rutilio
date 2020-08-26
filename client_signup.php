0a5054ba6a27e01bc127ed0c506d8c5ec08f52db
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2020-08-26 13:46:49 -0500

    Add files via upload

--- client_signup.php
+++ client_signup.php
@@ -7,6 +7,10 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!-- Alertify -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+    <!-- Alertify Default theme -->
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css"/>
     <link rel="stylesheet" href="assets/css/global_sign.css">
 
     <title>TimeToFood | Sign up</title>
@@ -32,19 +36,27 @@
         </ul>
       </div>
     </nav>
-    <form>
-      <div class="form-group">
-        <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Username">
-      </div>
-      <div class="form-group">
-        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-      </div>
-      <div class="form-group">
-        <input type="text" class="form-control" id="exampleInputPhone1" placeholder="Phone">
-      </div>
-      <div class="form-group">
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-      </div>
+    <form id="signup-form">
+      <div class="form-group"><input class="form-control" id="username" type="text"
+      minlength="5" maxlength="50"
+      placeholder="Username" autocomplete="off"
+      autofocus required>
+      <i class="fas" style="display: none;"></i></div>
+      <div class="form-group"><input class="form-control" id="email" type="email"
+        minlength="5" maxlength="50"
+        placeholder="Email" autocomplete="off"
+        autofocus required>
+        <i class="fas" style="display: none;"></i></div>
+      <div class="form-group"><input class="form-control" id="phone" type="text"
+        minlength="10" maxlength="10"
+        placeholder="Phone" autocomplete="off"
+        autofocus required>
+        <i class="fas" style="display: none;"></i></div>
+      <div class="form-group"><input class="form-control" id="password" type="password"
+        minlength="5" maxlength="50"
+        placeholder="Password" autocomplete="off"
+        autofocus required>
+        <i class="fas" style="display: none;"></i></div>
       <button type="submit" class="btn btn-primary btn-block">Sign up</button>
       <small class="form-text text-muted pt-3">You already have an account?, click <a href="client_signin.php">here</a>.</small>
     </form>
@@ -54,5 +66,11 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Alertify -->
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+    <script src="assets/js/global_control.js"></script>
+    <script src="assets/js/client_signup.js"></script>
   </body>
 </html>
